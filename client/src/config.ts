82cf03cdfa4a44f2eedae28bc4c874d4b6aeb30c
srikanthstorm <srikanthnaidu512@gmail.com>
2020-04-03 18:32:59 +0530

    feat: serverless TodoTasks

---
+++ client/src/config.ts
@@ -0,0 +1,7 @@
+export const apiEndpoint = 'https://xvauh3s6zd.execute-api.us-east-1.amazonaws.com/dev'
+
+export const authConfig = {
+  domain: 'dev-l68daul0.auth0.com',
+  clientId: 'X9Jx6U6cEBD7TnI3ZY1noYy5YA7ltlZB',
+  callbackUrl: 'http://localhost:3000/callback'
+}
